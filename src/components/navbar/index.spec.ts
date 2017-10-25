d9d135f0f218d63269ed103f864face657a82f12
paul hoover <ph@selectablemedia.com>
2017-10-25 12:53:54 -0400

    getting unit and e2e testing initial set-up working

---
+++ src/components/navbar/index.spec.ts
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import BaseButton from "./index";
+import { expect } from "chai";
+import { routes } from "../../routes";
+
+function getRenderedClassText(Component, propsData, className) {
+    const Ctor = Vue.extend(Component);
+    const vm = new Ctor({ propsData: propsData }).$mount();
+    return vm.$el.querySelector(className).textContent;
+}
+
+const TEXT_CONTENT: string = "home items labels";
+
+describe("foot.vue", () => {
+    it("should render nav bar links correctly", () => {
+        // Vue.use(VueRouter);
+        // const router = new VueRouter({
+        //     routes: routes
+        // });
+        expect(getRenderedClassText(BaseButton, {
+            version: "1.0.0",
+            build: "1",
+            // router: router
+        }, ".navbar-header"))
+            .to.equal(TEXT_CONTENT);
+    });
+});
--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -11,9 +11,8 @@ module.exports = {
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 2)
+      .assert.elementPresent('.view-home')
+      .assert.containsText('h2', 'home page')
       .end()
   }
 }
